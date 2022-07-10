db315196a73ce200a45ef890922cd8608b266297
LakelChester <lakel@chesterassociate.com>
2022-07-09 18:40:41 -0700

    commit tree 8c9ad207086f492c9136cfe9c04f400c8403392a

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const mainElement = document.querySelector("#main").remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+newHeader.setAttribute = "h1victory";
+newHeader.innerHTML = "Ashley is the champion";
